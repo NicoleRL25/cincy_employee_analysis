396f669cb4b486757c53330f4cf84280ef98d3bb
nikki225 <40606679+nikki225@users.noreply.github.com>
2020-12-30 21:13:46 -0500

    completed hypothesis testing on the rate of women in management

--- code/dashboard.py
+++ code/dashboard.py
@@ -87,7 +87,7 @@ emps['annual_rt']=emps.annual_rt.astype('float')
 
 
 
-#####Data Visualization
+#####Data Visualization#####
 
 
 #what is the composition of the workforce?
@@ -100,6 +100,57 @@ emps_by_paygroup.plot.pie(ax=ax,title='Employees by Pay Group',
                           wedgeprops={'width':0.3},
                           startangle=45)
 ax.set_ylabel(None)
+
+#are women and men equally represented at the management level?
+#28% of female and 19% of male employees are in management positions
+count_women=emps.sex.value_counts()['Female']
+count_men=emps.sex.value_counts()['Male']
+female_managers=emps.loc[(emps.paygroup_label=='Management')
+                         &(emps.sex=='Female'),'name'].count()
+male_managers=emps.loc[(emps.paygroup_label=='Management')
+                         &(emps.sex=='Male'),'name'].count()
+
+
+successes=np.array([female_managers,male_managers])
+samples=np.array([count_women,count_men])
+
+p_value=sm.stats.proportions_ztest(successes,samples,alternative='larger')[1]
+
+if p_value<0.05:
+    print('We reject the null hypothesis that women and men are represented'
+          'equally at the management level')
+else:
+    print('We fail to reject the null hypothesis that women are in management '
+          'at a rate lower than men')
+
+#does this hypotheis hold when we exclude part-time employees?
+fte_by_paygroup=(emps.loc[emps.full_time=='Full-Time']
+                 .pivot_table(index='paygroup_label',columns='sex',
+                              values='name',aggfunc='count',margins=True))
+
+
+count_women_full_time=fte_by_paygroup.loc[('All','Female')]
+
+
+full_time_female_mgrs=fte_by_paygroup.loc[('Management','Female')]
+
+count_men_full_time=fte_by_paygroup.loc[('All','Male')]
+
+full_time_male_mgrs=fte_by_paygroup.loc[('Management','Male')]
+
+successes=np.array([full_time_female_mgrs,full_time_male_mgrs])
+
+samples=np.array([count_women_full_time,count_men_full_time])
+
+p_value=sm.stats.proportions_ztest(successes,samples,alternative='larger')[1]
+
+if p_value<0.05:
+    print('We reject the null hypothesis that women and men are represented'
+          'equally at the management level')
+else:
+    print('We fail to reject the null hypothesis that women '
+          'are in management levels at a rate lower than men')
+
 
 #Employees in the General Pay Group make up approx. 48% of the workforce
 #The 10 roles below account for roughly 50% of the employees in that group
@@ -115,9 +166,10 @@ ax1.patches[9].set_color('darkorange')
 #The majority of employees are between 41 and 60 years of age
 #plot of the age distribution
 fig2,ax2=plt.subplots()
-emps.age_range.value_counts(sort=False).plot.bar(ax=ax,
-                                                 title='Age Distribution')
-ax2.set_xticklabels(labels=ax2.get_xticklabels(),rotation='horizontal')
+emps.age_range.value_counts(sort=False).plot.bar(ax=ax2,
+                                                 title='Age Distribution',
+                                                 rot=0)
+
 
 #The workforce is roughly 60% male
 #plot of employee gender distribution
@@ -130,6 +182,7 @@ ax3.yaxis.set_major_formatter(mtick.PercentFormatter(1))
 #When looking at a breakdown of job category by gender, we see that there
 #is an underrepresentation of women as Technicians, Skilled Craft Workers and 
 #Protective Service Workers (Police and Firefighters)
+#and an overrepresentation in administrative support
 job_class_by_gender=emps.pivot_table(index='eeo_job_class',values='name'
                                      , columns='sex',aggfunc='count')
                                      
@@ -137,22 +190,23 @@ job_class_by_gender=emps.pivot_table(index='eeo_job_class',values='name'
 job_class_by_gender_pct=job_class_by_gender.div(job_class_by_gender.sum(axis=1)
                                                 ,axis=0)
 
-fig8,ax8=plt.subplots(figsize=(9,7))
-job_class_by_gender_pct.plot.barh(stacked=True,cmap='tab20c',ax=ax8
+fig4,ax4=plt.subplots(figsize=(9,7))
+job_class_by_gender_pct.plot.barh(stacked=True,cmap='tab20c',ax=ax4
                                   ,title='Gender by Job Category')
-ax8.xaxis.set_major_formatter(mtick.PercentFormatter(1))
-ax8.legend(bbox_to_anchor=(1.1,1))
+ax4.xaxis.set_major_formatter(mtick.PercentFormatter(1))
+ax4.legend(bbox_to_anchor=(1.1,1))
+ax4.axvline(x=.5, color='red')
 
 
 #plot of racial demographics
-fig4,ax4=plt.subplots(figsize=(8,6))
-emps.race.value_counts().plot.pie(cmap='tab20c',ax=ax4
+fig5,ax5=plt.subplots(figsize=(8,6))
+emps.race.value_counts().plot.pie(cmap='tab20c',ax=ax5
                                   ,radius=1.5,wedgeprops={'width':.5}
                                   ,labeldistance=None
                                   ,autopct='%1.1f%%'
                                   ,pctdistance=1.2)
-ax4.legend(bbox_to_anchor=(1.2,.75))
-fig4.suptitle('Racial Demographics')
+ax5.legend(bbox_to_anchor=(1.2,.75))
+fig5.suptitle('Racial Demographics')
 
 job_class_by_race=emps.pivot_table(index='eeo_job_class',values='name'
                                      , columns='race',aggfunc='count')
@@ -164,35 +218,35 @@ job_class_by_race_pct=job_class_by_race.div(job_class_by_race.sum(axis=1)
                                                 ,axis=0)
 
 job_class_by_race_pct=job_class_by_race_pct.reindex(columns=race_col_list)
-fig9,ax9=plt.subplots(figsize=(9,7))
-job_class_by_race_pct.plot.barh(stacked=True,cmap='tab20c',ax=ax9
-                                  ,title='Gender by Job Category')
-ax9.xaxis.set_major_formatter(mtick.PercentFormatter(1))
-ax9.legend(bbox_to_anchor=(1.1,1))
+fig6,ax6=plt.subplots(figsize=(9,7))
+job_class_by_race_pct.plot.barh(stacked=True,cmap='tab20c',ax=ax6
+                                  ,title='Race by Job Category')
+ax6.xaxis.set_major_formatter(mtick.PercentFormatter(1))
+ax6.legend(bbox_to_anchor=(1.1,1))
 
 
 #plots tenure
-fig5,ax5=plt.subplots(3,1)
-sns.histplot(emps.tenure,kde=True,ax=ax4)
-ax5.set_title('Tenure Distribution')
+fig7,ax7=plt.subplots()
+sns.histplot(emps.tenure,kde=True,ax=ax7)
+ax7.set_title('Tenure Distribution')
 
 
 #plot of salary distribution
-fig6,ax6=plt.subplots()
-sns.histplot(emps.annual_rt,kde=True,ax=ax6)
-ax6.set_title('Salary Distribution')
-ax6.set_xlabel('Salary')
+fig8,ax8=plt.subplots()
+sns.histplot(emps.annual_rt,kde=True,ax=ax8)
+ax8.set_title('Salary Distribution')
+ax8.set_xlabel('Salary')
 
 #plot of full-time vs part-time employees
-fig7,ax7=plt.subplots()
+fig9,ax9=plt.subplots()
 emps.full_time.value_counts(normalize=True).plot.pie(radius=1.3
-                                                     ,ax=ax7
+                                                     ,ax=ax9
                                                      ,wedgeprops={'width':.3}
                                                      ,labeldistance=None
                                                      ,cmap='summer')
-ax7.set_title('Full_Time vs Part-Time',pad=20)
-ax7.set_ylabel('')
-ax7.legend(bbox_to_anchor=(1.25,.65))
+ax9.set_title('Full_Time vs Part-Time',pad=20)
+ax9.set_ylabel('')
+ax9.legend(bbox_to_anchor=(1.25,.65))
 
 
 
